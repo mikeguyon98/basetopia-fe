88b58c3acecfa34dcf636e2fb49533900e3234cf
Michael Guyon <mikeguyon98@gmail.com>
2024-12-29 14:11:12 -0500

    Moves to .env

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ0fjCXXTU4EJxtJ_s4v1qOFXYPvaM2EM",
-  authDomain: "basetopia-b9302.firebaseapp.com",
-  projectId: "basetopia-b9302",
-  storageBucket: "basetopia-b9302.firebasestorage.app",
-  messagingSenderId: "421858698882",
-  appId: "1:421858698882:web:cac0c799835ea05126769b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
